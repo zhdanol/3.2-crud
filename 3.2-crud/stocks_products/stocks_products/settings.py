a64dc9fe7b30a58ecf20c5d63dd9ef92ec6edbea
zhdanol <olegzdanov87@gmail.com>
2025-09-15 18:45:15 +0700

    docker

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'netology_stocks_products',
         'USER': 'postgres',
         'PASSWORD': '1589',
